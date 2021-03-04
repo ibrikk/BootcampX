a36ada59bdade4dccc21a03344e4cf78e5ead612
ibrikk <ibrahim.khalilov@yahoo.com>
2021-03-04 04:45:52 +0000

    add protection by hashing values out of the query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+const values = [process.argv[2]];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id 
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name = $1
+GROUP BY teacher, cohort
+ORDER BY teacher;`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
